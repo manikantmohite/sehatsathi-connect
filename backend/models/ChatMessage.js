4901453f9dc2dee97a86b865cfe9bc632706b299
Manikant Mohite <mohitemanikant@gmail.com>
2025-09-02 02:14:15 +0530

    Delete backend directory

--- backend/models/ChatMessage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const ChatMessageSchema = new mongoose.Schema({
-user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-message: String,
-fromBot: { type: Boolean, default: false },
-meta: Object,
-createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('ChatMessage', ChatMessageSchema);
--- backend/routes/chat.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/authMiddleware');
-const { postMessage, getHistory } = require('../controllers/chatController');
-router.get('/history', auth, getHistory);
-router.post('/message', auth, postMessage);
-module.exports = router;
